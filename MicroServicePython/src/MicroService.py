807458a7b95946d04537803f754c1822dcc344c7
pmayavi <pmayav@eafit.edu.co>
2023-03-27 16:25:45 -0500

    Funciona

--- MicroServicePython/src/MicroService.py
+++ MicroServicePython/src/MicroService.py
@@ -18,7 +18,7 @@ class MicroService(Service_pb2_grpc.MicroServiceServicer):
         file = open("log.txt", "a")
         file.write(item)
         file.close()
-        sleep(5)
+        sleep(1)
         return Service_pb2.ResponseInt(status=1, response=2)
 
     def SendInt(self, response, context):
@@ -28,7 +28,7 @@ class MicroService(Service_pb2_grpc.MicroServiceServicer):
         file = open("log.txt", "a")
         file.write(str(num))
         file.close()
-        sleep(5)
+        sleep(1)
         return Service_pb2.ResponseString(status=1, response="Hola ")
 
 
