ca67b48bc1bdd620d8c6da413fe0ecfa9c331d2c
pmayavi <pmayav@eafit.edu.co>
2023-03-27 15:59:20 -0500

    concadenate string

--- OnlineService/src/server.js
+++ OnlineService/src/server.js
@@ -21,7 +21,7 @@ const packageDefinition = protoLoader.loadSync(
 console.info("Consumer service is started...");
 
 function sendInt(sender, client, n) {
-  sender.SendInt({ num: n }, (err, data) => {
+  sender.SendInt({ num: str(n) }, (err, data) => {
     if (err) {
       console.log(err);
     } else {
